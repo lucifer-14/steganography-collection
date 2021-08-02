0ed86a0dab72391a9b28c6287ec36b4d2fb6172f
lucifer-14 <69714078+lucifer-14@users.noreply.github.com>
2021-08-02 10:32:49 +0630

    Update imagesteg.py
    New feature, bit index added.

--- Simple Image Steganography/imagesteg.py
+++ Simple Image Steganography/imagesteg.py
@@ -37,7 +37,7 @@ def to_binv2(data, passphrase):
         return ''.join(format(i, "08b") for i in to_ascii)
 
 
-def encode(image_name, secret_data, passphrase):
+def encode(image_name, secret_data, passphrase, bit_index):
     # read the image
     image = cv2.imread(image_name)
     # maximum bytes to encode
@@ -51,35 +51,32 @@ def encode(image_name, secret_data, passphrase):
     data_index = 0
     # convert data to binary version 2 (check passphrase)
     binary_secret_data = to_binv2(secret_data, passphrase)
-    # print (binary_secret_data)
     # size of data to hide
     data_len = len(binary_secret_data)
-
     for row in image:
         for pixel in row:
             # convert RGB values to binary format
-            
             r, g, b = to_bin(pixel)
-            # modify the least significant bit only if there is still data to store
+            # modify pixel RGB bits only if there is still data to store
             if data_index < data_len:
-                # least significant red pixel bit
-                pixel[0] = int(r[:-1] + binary_secret_data[data_index], 2)
+                # editing red pixel bit
+                pixel[0] = int(r[:bit_index] + binary_secret_data[data_index] + r[bit_index+1:], 2)
                 data_index += 1
             if data_index < data_len:
-                # least significant green pixel bit
-                pixel[1] = int(g[:-1] + binary_secret_data[data_index], 2)
+                # editing green pixel bit
+                pixel[1] = int(g[:bit_index] + binary_secret_data[data_index] + g[bit_index+1:], 2)
                 data_index += 1
             if data_index < data_len:
-                # least significant blue pixel bit
-                pixel[2] = int(b[:-1] + binary_secret_data[data_index], 2)
+                # editing blue pixel bit
+                pixel[2] = int(b[:bit_index] + binary_secret_data[data_index] + b[bit_index+1:], 2)
                 data_index += 1
-            # if data is encoded, just break out of the loop
+            # if data is encoded, break out of the loop
             if data_index >= data_len:
                 break
     return image
 
 
-def decode(image_name, passphrase):
+def decode(image_name, passphrase, bit_index):
     print("[+] Decoding...")
     # read the image
     image = cv2.imread(image_name)
@@ -87,9 +84,9 @@ def decode(image_name, passphrase):
     for row in image:
         for pixel in row:
             r, g, b = to_bin(pixel)
-            binary_data += r[-1]
-            binary_data += g[-1]
-            binary_data += b[-1]
+            binary_data += r[bit_index]
+            binary_data += g[bit_index]
+            binary_data += b[bit_index]
     # split by 8-bits
     all_bytes = [ binary_data[i: i+8] for i in range(0, len(binary_data), 8) ]
     counter = 0
@@ -120,19 +117,31 @@ def decode(image_name, passphrase):
 
 
 if __name__ == "__main__":
+
+    bit_index = 7           # 0 - 7, 0 = most significant bit, 7 = least significant bit, default = 7 
+    passphrase="default"    # default value of passphrase = default (It's so safe right, LOL)
+
     parser = argparse.ArgumentParser(description="Steganography Image encoder/decoder, this Python scripts encode data within images. Encoding using passphrase is optional.")
     parser.add_argument("-t", "--text", help="The text data to encode into the image (ONLY for encoding)")
     parser.add_argument("-p", "--passphrase",  help="The passphrase for additional security (optional)")
     parser.add_argument("-e", "--encode", help="The image file to be encoded")
     parser.add_argument("-o", "--outfile", help="The output file name (ONLY for encoding, optional)")
     parser.add_argument("-d", "--decode", help="The image file to be decoded")
+    parser.add_argument("-b", "--bit_index", help="The bit index of pixel to encode or decode (0-7: 0 = most significant bit, 7 = least significant bit, default = 7")
     
     
     args = parser.parse_args()
     secret_data = args.text
-    passphrase=""
+    
     if args.passphrase:
         passphrase=args.passphrase
+    if args.bit_index:
+        bit_index=args.bit_index
+
+    bit_index=int(bit_index)
+    # change to default bit index if it is invalid bit index
+    if not (0<=bit_index<=7) :
+        bit_index = 7       
 
     if args.encode:
         # if the encode argument is specified
@@ -149,7 +158,7 @@ if __name__ == "__main__":
             outfile=f"{args.outfile}.{ext}"
         output_image = os.path.join(path, outfile)
         # encode the data into the image
-        encoded_image = encode(image_name=input_image, secret_data=secret_data, passphrase=passphrase)
+        encoded_image = encode(image_name=input_image, secret_data=secret_data, passphrase=passphrase, bit_index=bit_index)
         # save the output image (encoded image)
         cv2.imwrite(output_image, encoded_image)
         print("[+] Saved encoded image.")
@@ -157,5 +166,5 @@ if __name__ == "__main__":
     if args.decode:
         input_image = args.decode
         # decode the secret data from the image
-        decoded_data = decode(image_name=input_image, passphrase=passphrase)
+        decoded_data = decode(image_name=input_image, passphrase=passphrase, bit_index=bit_index)
         print("[+] Decoded data:", decoded_data)
